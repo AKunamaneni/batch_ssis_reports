555b25c7cb2eac5bb23c95627c2f987f86b41e58
akunam <amulakunam@optum.com>
2020-01-07 11:30:02 -0600

    updates

--- src/main/java/com/uhg/reports/batch/entity/DashboardEmbeddedReportProperty.java
+++ src/main/java/com/uhg/reports/batch/entity/DashboardEmbeddedReportProperty.java
@@ -1,9 +1,11 @@
 package com.uhg.reports.batch.entity;
 
-import lombok.Data;
-
-import javax.persistence.*;
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(
--- src/main/java/com/uhg/reports/batch/processor/ReportsProcessor.java
+++ src/main/java/com/uhg/reports/batch/processor/ReportsProcessor.java
@@ -1,33 +1,26 @@
 package com.uhg.reports.batch.processor;
 
-import com.uhg.reports.batch.entity.DashboardEmbeddedReport;
-import com.uhg.reports.batch.entity.DashboardEmbeddedReportProperty;
-import com.uhg.reports.batch.repository.DashboardEmbeddedReportPropertyRepository;
-import com.uhg.reports.batch.repository.DashboardEmbeddedReportRepository;
-import gui.ava.html.image.generator.HtmlImageGenerator;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
-import javax.imageio.ImageIO;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import com.uhg.reports.batch.entity.DashboardEmbeddedReport;
+import com.uhg.reports.batch.entity.DashboardEmbeddedReportProperty;
+import com.uhg.reports.batch.repository.DashboardEmbeddedReportPropertyRepository;
+import com.uhg.reports.batch.repository.DashboardEmbeddedReportRepository;
 
 @Component
 @EnableScheduling
@@ -54,28 +47,21 @@ public class ReportsProcessor {
                 BufferedImage bufferedImage = null;
                 ByteArrayOutputStream byteArrayOutputStream = null;
                 try {
-                    HtmlImageGenerator htmlImageGenerator = new HtmlImageGenerator();
-                    htmlImageGenerator.loadUrl(dashboardEmbeddedReportProperty.getReportLink());
-                    bufferedImage = htmlImageGenerator.getBufferedImage();
-                	// get the byte array of the image (as jpeg)  
+                	File imgPath = new File("C:\\Users\\akunam\\Desktop",dashboardEmbeddedReportProperty.getReportname()+".png");  
+                	bufferedImage = ImageIO.read(imgPath);
                 	byteArrayOutputStream = new ByteArrayOutputStream();
                 	ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
                     DashboardEmbeddedReport dashboardEmbeddedReport = new DashboardEmbeddedReport();
                     dashboardEmbeddedReport.setImage(byteArrayOutputStream.toByteArray());
                     dashboardEmbeddedReport.setReportname(dashboardEmbeddedReportProperty.getReportname());
-                    dashboardEmbeddedReport.setWeekendDate("2020-01-04");
+                    SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");  
+                     dashboardEmbeddedReport.setWeekendDate(sdfDate.format(new Date()));                    
                     byteArrayOutputStream.flush();
                     reportImages.add(dashboardEmbeddedReport);
 
                 } catch (IOException e) {
                     e.printStackTrace();
-                } //finally {
-//                    try {
-//                        byteArrayOutputStream.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
+                } 
 
             });
         }
